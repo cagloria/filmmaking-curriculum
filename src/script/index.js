38e63a6d77be51edb61d16f35e9b1e680588e5dd
cagloria <33134245+cagloria@users.noreply.github.com>
2024-03-08 12:39:01 -0700

    Cleaned up JS

--- src/script/index.js
+++ src/script/index.js
@@ -1,71 +1,20 @@
-const media = window.matchMedia("(max-width: 768px)");
+import { toggleNav, setNavState, media } from "./navigation.js";
 
-window.addEventListener("load", (e) => {
+window.addEventListener("load", () => {
+    const navButton = document.getElementById("nav-menu-button");
+    const navOverlay = document.getElementById("nav-overlay");
+
+    [navButton, navOverlay].forEach((element) => {
+        element.addEventListener("click", () => {
+            toggleNav();
+        });
+    });
+
     setNavState(!media.matches);
 
     document.addEventListener("scroll", (e) => {
-        if (
-            document
-                .getElementById("nav-menu-button")
-                .getAttribute("aria-expanded") === "true"
-        ) {
+        if (navButton.getAttribute("aria-expanded") === "true") {
             toggleNav();
         }
     });
 });
-
-media.onchange = (e) => {
-    const button = document.getElementById("nav-menu-button");
-
-    setNavState(!e.matches);
-
-    // Closes navigation button on changing to desktop layout
-    if (button.getAttribute("aria-expanded") === "true") {
-        button.setAttribute("aria-expanded", "false");
-        button.setAttribute("aria-label", "Open navigation");
-    }
-};
-
-/**
- * Toggle navigation between desktop and mobile
- */
-function toggleNav() {
-    const button = document.getElementById("nav-menu-button");
-
-    if (button.getAttribute("aria-expanded") === "true") {
-        button.setAttribute("aria-expanded", "false");
-        button.setAttribute("aria-label", "Open navigation");
-        setNavLinksTabindex(false);
-    } else {
-        button.setAttribute("aria-expanded", "true");
-        button.setAttribute("aria-label", "Close navigation");
-        setNavLinksTabindex(true);
-    }
-}
-
-/**
- * Set the initial accessibility attributes for navigation button and links.
- * @param {boolean} isDesktop   Page is on desktop layout
- */
-function setNavState(isDesktop) {
-    const button = document.getElementById("nav-menu-button");
-
-    button.setAttribute("aria-hidden", `${isDesktop}`);
-    button.setAttribute("tabindex", isDesktop ? "-1" : "0");
-    button.setAttribute("aria-label", "Open navigation");
-
-    setNavLinksTabindex(isDesktop);
-}
-
-/**
- * Set the tabindex of navigation links.
- * @param {boolean} isDesktopOrExpandedNav  The page is on desktop layout or
- *                                          navigation is expanded
- */
-function setNavLinksTabindex(isDesktopOrExpandedNav) {
-    const navLinks = document.querySelectorAll("nav a");
-
-    navLinks.forEach((link) => {
-        link.setAttribute("tabindex", isDesktopOrExpandedNav ? "0" : "-1");
-    });
-}
